d28894acede46b8f79449b444b1cfadc94fe02b0
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2023-01-21 23:47:38 +0200

    Setting up database connection

--- citybike-backend/index.js
+++ citybike-backend/index.js
@@ -1,11 +1,17 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+const { PORT } = require('./utils/config')
+const { connectToDatabase } = require('./utils/db')
 
 app.get('/', async (req, res) => {
   res.send('Starting a project')
 })
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
