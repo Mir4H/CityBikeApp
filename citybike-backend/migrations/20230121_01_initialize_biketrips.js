562d89da69b43d5921af429fb57d750e3fbdc500
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2023-01-27 09:04:03 +0200

    getting the initial data on biketrips

--- citybike-backend/migrations/20230121_01_initialize_biketrips.js
+++ citybike-backend/migrations/20230121_01_initialize_biketrips.js
@@ -1,4 +1,51 @@
 const { DataTypes } = require('sequelize')
+const axios = require('axios')
+const csv = require('csvtojson')
+const Biketrip = require('../models/biketrip')
+
+const getcsvdata = async (dataUrl) => {
+  const file = await axios({
+    url: dataUrl,
+    method: 'GET',
+    responseType: 'text'
+  })
+
+  const jsonArray = await csv({
+    noheader: false,
+    headers: [
+      'departureTime',
+      'returnTime',
+      'departureStationId',
+      'departureStationName',
+      'returnStationId',
+      'returnStationName',
+      'coveredDistance',
+      'duration'
+    ]
+  }).fromString(file.data)
+
+  let nro = 1000
+  let offset = 0
+  while (offset < jsonArray.length) {
+    const chunk = jsonArray
+      .slice(offset, nro)
+      .filter((item) => item.coveredDistance > 10 && item.duration > 10)
+    try {
+      await Biketrip.bulkCreate(chunk, { validate: true })
+      console.log(`Added rows from ${offset} to ${nro}`)
+      offset = nro
+      nro += 1000
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+const dataUrls = [
+  'https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv',
+  'https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv',
+  'https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv'
+]
 
 module.exports = {
   up: async ({ context: queryInterface }) => {
@@ -53,6 +100,12 @@ module.exports = {
         }
       }
     })
+    await Promise.all(
+      dataUrls.map(async (dataUrl) => {
+        console.log('Getting data, this may take a while, please wait...')
+        await getcsvdata(dataUrl)
+      })
+    )
   },
   down: async ({ context: queryInterface }) => {
     await queryInterface.dropTable('biketrips')
